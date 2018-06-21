03666b78752bc86bd85431c0f41bb9b6fd2e0023
20171105120 <476625879@qq.com>
2018-06-21 08:44:56 +0800

    Initial Commit

---
+++ std/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  std
+//
+//  Created by 刘洋博 on 2018/6/21.
+//  Copyright © 2018年 刘洋博. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
